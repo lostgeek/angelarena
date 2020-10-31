d0e72c8e1826898044570375bc6377f7545d629c
Oguz Han Asnaz <github@lostgeek.de>
2020-10-31 12:42:34 +0100

    SSS tournament implementation complete

--- angelarena/system.py
+++ angelarena/system.py
@@ -2,6 +2,9 @@ import uuid
 import itertools
 import random
 from enum import Enum
+import networkx as nx
+import numpy as np
+import pandas as pd
 
 class Player(object):
     def __init__(self, member):
@@ -16,164 +19,177 @@ class SSSPlayer(Player):
     def __init__(self, member):
         super().__init__(member)
         self.matches = []
-        self.corp_games = 0
-        self.runner_games = 0
-        self.losses = 0
+        self.side_balance = 0 # pos = more corp than runner
+        self.score = 0
+        self.byes = 0
 
     def __str__(self):
-        return f"<..{str(self.id)[-3:]}: {len(self.matches)-self.losses}-{self.losses}>"
+        return f"<..{str(self.id)[-3:]}: {self.score}>"
+
+    def __repr__(self):
+        return self.__str__()
+
+class SSSByePlayer(SSSPlayer):
+    def __init__(self):
+        super().__init__(None)
+
+    def __getattribute__(self, attr):
+        if attr == "score":
+            return -1
+        elif attr == "side_balance":
+            return 0
+        else:
+            return super().__getattribute__(attr)
+
+    def __str__(self):
+        return f"<BYE>"
+
+    def __repr__(self):
+        return self.__str__()
 
 class Match(object):
     pass
 
 class SSSMatch(Match):
-    def __init__(self, p1, p2, round, reason=None):
+    def __init__(self, corp, runner, round_number):
+        self.corp = corp
+        self.runner = runner
+        self.round_number = round_number
+
         self.result = SSSResults.open
-        self.round = round
-        self.bye = False
-        self.reason = reason
 
-        # bye
-        if not p2:
-            self.bye = True
-            if p1.corp_games > p1.runner_games:
-                self.runner = p1
-                self.corp = None
-                self.result = SSSResults.win_runner
-            elif p1.corp_games < p1.runner_games:
-                self.runner = None
-                self.corp = p1
-                self.result = SSSResults.win_corp
-            else:
-                self.reason = "Side was randomized, because of balanced record"
-                if bool(random.getrandbits(1)):
-                    self.runner = p1
-                    self.corp = None
-                    self.result = SSSResults.win_runner
-                else:
-                    self.runner = None
-                    self.corp = p1
-                    self.result = SSSResults.win_corp
-
-        # normal match
+    def opponent(self, p):
+        if p == self.corp:
+            return self.runner
+        elif p == self.runner:
+            return self.corp
         else:
-            if p1.corp_games > p2.corp_games:
-                self.runner = p1
-                self.corp = p2
-            elif p1.corp_games < p2.corp_games:
-                self.runner = p2
-                self.corp = p1
-            else: # both players played the same number of corp games
-                self.reason = "Sides were randomized, because both players had same record"
-                if bool(random.getrandbits(1)):
-                    self.runner = p1
-                    self.corp = p2
-                else:
-                    self.runner = p2
-                    self.corp = p1
+            raise Exception('Player not in match')
 
     def __contains__(self, p):
         return p == self.corp or p == self.runner
 
     def __str__(self):
-        if not self.corp:
-            cname = "<BYE>"
-        else:
-            cname = str(self.corp)
-
-        if not self.runner:
-            rname = "<BYE>"
-        else:
-            rname = str(self.runner)
-
         if self.result == SSSResults.win_corp:
             result = "c"
         elif self.result == SSSResults.win_runner:
             result = "r"
         elif self.result == SSSResults.draw:
             result = "d"
+        elif self.result == SSSResults.bye:
+            result = "b"
         elif self.result == SSSResults.open:
             result = "_"
 
-        reason = ""
-        if self.reason:
-            reason = f"rea: {reason}"
+        return f"[c:{str(self.corp)} r:{str(self.runner)} res:{result}]"
 
-        return f"[c:{cname} r:{rname} res:{result}{reason}]"
+    def __repr__(self):
+        return self.__str__()
 
 class SSSResults(Enum):
     open = 0
     win_corp = 1
     win_runner = 2
     draw = 3
+    bye = 4
 
 class System(object):
     pass
 
 class SSSSystem(System):
+    bye_player = SSSByePlayer()
+
     def __init__(self, max_losses = 3, max_rounds = None):
         self.players = []
+        self.eliminated = []
         self.dropped = []
         self.rounds = []
         self.max_losses = max_losses
         self.max_rounds = max_rounds
 
+        self.score_factor = 1
+        self.repair_penalty = 10000
+
     def add_new_player(self, member):
         p = SSSPlayer(member)
         self.players.append(p)
 
+    def make_score_penalty_array(self):
+        df = np.array([[player.score for player in self.players]])
+        df = abs(df - df.T)*self.score_factor
+        return df
+
+    def make_side_penalty_array(self):
+        df = np.array([[player.side_balance for player in self.players]])
+        same_bias = ((df * df.T)> 0)
+        np.fill_diagonal(same_bias,0)
+        min_bias = np.minimum(abs(df), abs(df.T))
+        return (8**(min_bias))*same_bias
+
+    def make_repair_penalty_array(self):
+        penalty = np.zeros((len(self.players), len(self.players)))
+        for i in range(len(self.players)):
+            for j in range(i, len(self.players)):
+                p1 = self.players[i]
+                p2 = self.players[j]
+                penalty[j][i] = len(list(filter(lambda match: p2 in match, p1.matches)))
+        penalty = penalty + penalty.T - np.diag(np.diag(penalty)) # fill other triangle
+        np.fill_diagonal(penalty, 0)
+        penalty *= self.repair_penalty
+        return penalty
+
+    def make_bye_bonus_array(self):
+        df = np.array([[player.byes for player in self.players]])
+        bonus = abs(df - df.T)+1
+        return bonus
+
+    def make_pairings_matrix(self):
+        pairing_matrix = 20000 \
+                - self.make_score_penalty_array() \
+                - self.make_side_penalty_array() \
+                - self.make_repair_penalty_array()
+        pairing_matrix *= self.make_bye_bonus_array()
+        pairing_matrix += np.random.randint(0,10,size=(len(self.players), len(self.players)))
+        pairing_matrix = pd.DataFrame(pairing_matrix, \
+                index = [player for player in self.players], \
+                columns = [player for player in self.players])
+        return pairing_matrix
+
     def pair_new_round(self):
-        groups = itertools.groupby(self.players, key = lambda p: p.losses)
-        unpaired = {}
-        for k, g in groups:
-            unpaired[k] = list(g)
-
-        # remove players with max number of losses
-        if self.max_losses in unpaired:
-            del unpaired[self.max_losses]
-
         round = []
         round_number = len(self.rounds)+1
-        losses = 0 # current loss level to be paired
-        while unpaired:
-            # no players on loss level
-            if not losses in unpaired:
-                losses += 1
-                continue
 
-            # grab first player
-            p1 = unpaired[losses].pop()
-            if not unpaired[losses]:
-                del unpaired[losses]
+        if self.bye_player in self.players:
+            self.players.remove(self.bye_player)
 
-            # no players left to pair: give out a bye
-            if not unpaired:
-                match = SSSMatch(p1, None, round_number, reason="No players left to match")
-                p2 = None
+        if len(self.players) % 2 == 1:
+            self.players.append(self.bye_player)
 
-            # search second player
+        pairing_matrix = self.make_pairings_matrix()
+        pairing_graph = nx.convert_matrix.from_pandas_adjacency(pairing_matrix)
+        pairings = nx.max_weight_matching(pairing_graph,maxcardinality=True)
+
+        for p in pairings:
+            p0corps = (p[1].side_balance-p[0].side_balance > 0)
+            if p[1].side_balance-p[0].side_balance == 0:
+                p0corps = bool(random.getrandbits(1))
+
+            if p0corps:
+                corp = p[0]
+                runner = p[1]
             else:
-                match_losses = losses
-                match_i = 0
-                p2 = None
-                while not p2:
-                    # no other players found with this number of losses, trying to pair down
-                    if not match_losses in unpaired:
-                        match_losses += 1
-                    else:
-                        p2 = unpaired[match_losses][match_i]
+                corp = p[1]
+                runner = p[0]
 
-                        #XXX: room for checks
-
-                        unpaired[match_losses].pop(match_i)
-                        if not unpaired[match_losses]:
-                            del unpaired[match_losses]
-                match = SSSMatch(p1, p2, round_number)
+            match = SSSMatch(corp, runner, round_number)
             round.append(match)
-            p1.matches.append(match)
-            if p2:
-                p2.matches.append(match)
+            corp.side_balance += 1
+            corp.matches.append(match)
+            runner.side_balance -= 1
+            runner.matches.append(match)
 
+            if self.bye_player in match:
+                match.result = SSSResults.bye
 
         self.rounds.append(round)
 
@@ -183,13 +199,28 @@ class SSSSystem(System):
         if next(filter(lambda match: match.result == SSSResults.open, round), None):
             raise Exception("Tried to finish round with open results.")
 
+        if self.bye_player in self.players:
+            self.players.remove(self.bye_player)
+
         for match in round:
             if match.result == SSSResults.win_corp:
-                match.runner.losses += 1
+                match.corp.score += 1
             elif match.result == SSSResults.win_runner:
-                match.corp.losses += 1
-            elif match.result == SSSResults.draw:
-                match.corp.losses += 1
-                match.runner.losses += 1
-            else:
-                raise Exception("Tried to finish round with open results. This should not have happened, leaving round in a broken state!")
+                match.runner.score += 1
+            elif match.result == SSSResults.bye:
+                player = match.opponent(self.bye_player)
+                player.byes += 1
+                player.score += 1
+
+        for player in self.players:
+            losses = len(self.rounds)-player.score
+            if losses > self.max_losses:
+                self.players.remove(player)
+                self.eliminated.append(player)
+
+    def drop_player(self, player):
+        if not player in self.players:
+            raise Exception("Player not in system")
+
+        self.players.remove(player)
+        self.dropped.append(player)
--- tests/test_sss_system.py
+++ tests/test_sss_system.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 from angelarena import system
 
@@ -57,11 +58,79 @@ class TestSSSSystem(unittest.TestCase):
             self.assertEqual(sum(1 for match in round if player in match), 1,
                     "Found only one match per player")
 
-        bye = next(filter(lambda m: None in m, round), None)
+        bye = next(filter(lambda m: sys.bye_player in m, round), None)
         self.assertIsNotNone(bye,
                 "Bye was given out")
         self.assertIsNot(bye.result, system.SSSResults.open,
                 "Bye match is not marked as open")
+
+    def test_full_small_tournament_elimination(self):
+        """
+        Simulate small elimination tournament
+        """
+
+        sys = system.SSSSystem(3,None)
+        for i in range(15):
+            sys.add_new_player(None) # Passing None as discord member
+
+        for i in range(100):
+            sys.pair_new_round()
+            for m in sys.rounds[-1]:
+                if m.result == system.SSSResults.open:
+                    ran = random.randint(0, 99)
+                    if ran < 48:
+                        m.result = system.SSSResults.win_corp
+                    elif ran < 96:
+                        m.result = system.SSSResults.win_runner
+                    else:
+                        m.result = system.SSSResults.draw
+            sys.finish_round()
+            if len(sys.players) <= 1:
+                break
+
+        self.assertTrue(len(sys.rounds) < 100,
+                "Tournament ended naturally")
+
+        self.assertEqual(len(sys.players), 1,
+                "Tournament ended with a winner")
+
+    @unittest.skip("Takes long time")
+    def test_full_huge_tournament(self):
+        """
+        Simulate huge elimination tournament
+        """
+
+        sys = system.SSSSystem(3,None)
+        for i in range(512):
+            sys.add_new_player(None) # Passing None as discord member
+
+        for i in range(30):
+            sys.pair_new_round()
+            for m in sys.rounds[-1]:
+                if m.result == system.SSSResults.open:
+                    ran = random.randint(0, 99)
+                    if ran < 48:
+                        m.result = system.SSSResults.win_corp
+                    elif ran < 96:
+                        m.result = system.SSSResults.win_runner
+                    else:
+                        m.result = system.SSSResults.draw
+            sys.finish_round()
+
+            if len(sys.players) <= 1:
+                break
+
+            for p in sys.players:
+                if random.randint(0, 200) < i:
+                    sys.drop_player(p)
+
+            print(f"{i:02d}: p: {len(sys.players)} e: {len(sys.eliminated)} d: {len(sys.dropped)}")
+
+        self.assertTrue(len(sys.rounds) < 30,
+                "Tournament ended naturally")
+
+        self.assertEqual(len(sys.players), 1,
+                "Tournament ended with a winner")
 
 if __name__ == '__main__':
     unittest.main()
